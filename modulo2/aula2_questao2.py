af9912528fb5a7d5b1eecea3594f216609e111a0
Breno Bispo <63658113+BBispo368@users.noreply.github.com>
2024-05-23 13:45:59 -0300

    Add files via upload

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,16 @@
+var1 = "o resultado é"
+var2 = 10
+var3 = 3.5
+var4 = var2 + var3
+
+print(var1)
+print(type(var1))
+
+print(var2)
+print(type(var2))
+
+print(var3)
+print(type(var3))
+
+print(var4)
+print(type(var4))
---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,5 @@
+rendimento2 = (500.0 * 1.01) * 1.01
+rendimento3 = rendimento2 * 1.01
+
+print("Após 3 meses meu novo saldo é")
+print(rendimento3)
