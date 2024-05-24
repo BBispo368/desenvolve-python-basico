7b6d4dca7a5e37f54e284dbba4f5817ab4ee623b
Breno Bispo <63658113+BBispo368@users.noreply.github.com>
2024-05-24 13:15:40 -0300

    Update aula2_questao4.py

--- modulo2/aula2_questao4.py
+++ modulo2/aula2_questao4.py
@@ -1,5 +1,11 @@
-rendimento2 = (500.0 * 1.01) * 1.01
-rendimento3 = rendimento2 * 1.01
+saldo = 500
+juros = 1.01
 
-print("Após 3 meses meu novo saldo é")
-print(rendimento3)
+saldo = saldo*juros
+print(saldo)
+
+saldo = saldo*juros
+print(saldo)
+
+saldo = saldo*juros
+print(saldo)
