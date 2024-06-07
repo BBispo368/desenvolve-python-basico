8314dff76544aa4e66edf078e62ddcb69fae2c15
Breno Bispo <63658113+BBispo368@users.noreply.github.com>
2024-06-07 13:04:05 -0300

    Create aula1_questao1.py

---
+++ modulo5/aula1_questao1.py
@@ -0,0 +1,4 @@
+digito1 = float(input("Digite o primeiro numero: "))
+digito2 = float(input("Digite o segundo numero: "))
+
+print(f"A diferença absoluta entre os números é: {round(abs(digito1 - digito2), 2)}")
