2568454a84fb54a44b4c8bfd4c531bdfae09f484
Breno Bispo <63658113+BBispo368@users.noreply.github.com>
2024-06-17 12:53:37 -0300

    Update aula2_questao1.py

--- modulo6/aula2_questao1.py
+++ modulo6/aula2_questao1.py
@@ -1,6 +1,9 @@
 import random
-
 lista = []
+
 for i in range(20):
-    lista[i] = random.randint(-100,100)
-print(lista)
+    lista.append(random.randint(-100,100))
+print("Ordenada: ",sorted(lista))
+print("Original: ", lista)
+print("Maior valor: ", max(lista))
+print("Menor valor: ", min(lista))
